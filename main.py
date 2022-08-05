37df39b1751e61bd4f1c1c39464e157d5e9950c9
TAKANO Mitsuhiro <takano32@gmail.com>
2022-08-06 01:18:14 +0900

    reverse proxy to httpbin.org

--- main.py
+++ main.py
@@ -1,43 +1,39 @@
-from flask import Flask,request,redirect,Response
 import requests
-app = Flask(__name__)
-SITE_NAME = "http://MAIN_SITE_NAME"
+from flask import Flask, Response, redirect, request
 
-@app.route("/",methods=['GET','POST'])
+app = Flask(__name__)
+SITE_NAME = "http://httpbin.org"
+
+
+def _request(req: request, url: str):
+    if req.method == "GET":
+        resp = requests.get(url)
+        excluded_headers = ["content-encoding", "content-length", "transfer-encoding", "connection"]
+        headers = [(name, value) for (name, value) in resp.raw.headers.items() if name.lower() not in excluded_headers]
+        response = Response(resp.content, resp.status_code, headers)
+        return response
+    elif req.method == "POST":
+        resp = requests.post(url, data=req.form)
+        excluded_headers = ["content-encoding", "content-length", "transfer-encoding", "connection"]
+        headers = [(name, value) for (name, value) in resp.raw.headers.items() if name.lower() not in excluded_headers]
+        response = Response(resp.content, resp.status_code, headers)
+        return response
+    pass
+
+
+@app.route("/", methods=["GET", "POST"])
 def index():
     global SITE_NAME
-    if request.method=="GET":
-        resp = requests.get(f"{SITE_NAME}")
-        excluded_headers = ["content-encoding", "content-length", "transfer-encoding", "connection"]
-        headers = [(name, value) for (name, value) in  resp.raw.headers.items() if name.lower() not in excluded_headers]
-        response = Response(resp.content, resp.status_code, headers)
-        return response
-    elif request.method=="POST":
-        resp = requests.post(f"{SITE_NAME}",data=request.form)
-        excluded_headers = ["content-encoding", "content-length", "transfer-encoding", "connection"]
-        headers = [(name, value) for (name, value) in resp.raw.headers.items() if name.lower() not in excluded_headers]
-        response = Response(resp.content, resp.status_code, headers)
-        return response
+    url = f"{SITE_NAME}/"
+    return _request(request, url)
 
 
-
-@app.route("/<path:path>",methods=["GET","POST"])
+@app.route("/<path:path>", methods=["GET", "POST"])
 def proxy(path):
     global SITE_NAME
-    if request.method=="GET":
-        resp = requests.get(f"{SITE_NAME}{path}")
-        excluded_headers = ["content-encoding", "content-length", "transfer-encoding", "connection"]
-        headers = [(name, value) for (name, value) in  resp.raw.headers.items() if name.lower() not in excluded_headers]
-        response = Response(resp.content, resp.status_code, headers)
-        return response
-    elif request.method=="POST":
-        resp = requests.post(f"{SITE_NAME}{path}",data=request.form)
-        excluded_headers = ["content-encoding", "content-length", "transfer-encoding", "connection"]
-        headers = [(name, value) for (name, value) in resp.raw.headers.items() if name.lower() not in excluded_headers]
-        response = Response(resp.content, resp.status_code, headers)
-        return response
-
+    url = f"{SITE_NAME}/{path}"
+    return _request(request, url)
 
 
 if __name__ == "__main__":
-    app.run(debug = True,port=1337)
+    app.run(debug=True)
